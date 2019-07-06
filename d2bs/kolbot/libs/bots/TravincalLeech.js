b78a74a3feb493497c0179062aa6f890ffeb55d9
boddhisatra <31551141+boddhisatra@users.noreply.github.com>
2019-07-06 10:30:24 -0700

    Update TravincalLeech.js

--- d2bs/kolbot/libs/bots/TravincalLeech.js
+++ d2bs/kolbot/libs/bots/TravincalLeech.js
@@ -5,29 +5,39 @@
 */
 
 function TravincalLeech() {
+	var wait = true;
+	
+	addEventListener('copydata',
+		function ReceiveCopyData(mode, msg) {
+			if (mode === 83 && msg === "leave") {
+				wait = false;
+			}
+		}
+	)
+
 	Town.goToTown(3);
 	Town.move("portalspot");
 
-	while (!Misc.inMyParty(Config.Leader)) {
+	while (!Misc.inMyParty(Config.Leader) || !Pather.getPortal(83, Config.Leader)) {
 		delay(500);
 	}
 
-	while (Misc.inMyParty(Config.Leader)) {
-		if (me.inTown && Pather.getPortal(83, Config.Leader)) {
-			Pather.usePortal(83, Config.Leader);
-			Town.getCorpse();
+	if (me.inTown && Pather.getPortal(83, Config.Leader)) {
+		Pather.usePortal(83, Config.Leader);
+		Town.getCorpse();
+	}
+
+	if (me.mode === 17) {
+		me.revive();
+
+		while (!me.inTown) {
+			delay(100);
 		}
 
-		if (me.mode === 17) {
-			me.revive();
-
-			while (!me.inTown) {
-				delay(100);
-			}
-
-			Town.move("portalspot");
-		}
-
+		Town.move("portalspot");
+	}
+	
+	while (wait) {
 		delay(100);
 	}
 
